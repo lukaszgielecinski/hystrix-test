41826ee17ae28d1f20580ffad00f3eb98c1c8b7a
Lukasz Gielecinski <lukasz.gielecinski@gmail.com>
2017-06-23 00:09:53 +0100

    async calls

--- src/main/java/Command.java
+++ src/main/java/Command.java
@@ -1,13 +1,21 @@
-import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandProperties;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
+import com.netflix.hystrix.HystrixObservableCommand;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.nio.client.HttpAsyncClient;
+import rx.Observable;
+import rx.apache.http.ObservableHttp;
+import rx.apache.http.ObservableHttpResponse;
+import rx.functions.Action1;
+import rx.functions.Func0;
+import rx.functions.Func1;
+import rx.subjects.BehaviorSubject;
 
+import java.io.IOException;
 import java.util.Date;
 
-public class Command extends HystrixCommand<Boolean> {
+public class Command extends HystrixObservableCommand<Boolean> {
 
     protected Command() {
         super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("group"))
@@ -17,21 +25,57 @@ public class Command extends HystrixCommand<Boolean> {
                         .withExecutionTimeoutInMilliseconds(Starter.HYSTRIX_TIMEOUT)));
     }
 
-    @Override
-    protected Boolean run() throws Exception {
-        System.out.println("startRun: " + new Date());
-        HttpGet httpRequest = new HttpGet("http://localhost:" + Starter.PORT_NUMBER + "/2000");
-        HttpClient client = HttpClientBuilder.create()
-                .build();
-        client.execute(httpRequest);
+    public Observable<Boolean> request(String url) {
+        Func0<CloseableHttpAsyncClient> resourceFactory = () -> {
+            CloseableHttpAsyncClient client = HttpAsyncClients.createDefault();
+            client.start();
 
-        System.out.println("stopRun: " + new Date());
-        return true;
+            System.out.println(Thread.currentThread().getName() + " : Created and started the client.");
+            return client;
+        };
+
+        Func1<HttpAsyncClient, Observable<Boolean>> observableFactory = (client) -> {
+            System.out.println(Thread.currentThread().getName() + " : About to create Observable.");
+            return ObservableHttp.createGet(url, client).toObservable()
+                    .flatMap(observableResponse -> {
+                        System.out.println("here: " + new Date());
+                        Observable<Boolean> observable = observableResponse.getContent()
+                                .map(bytes -> {
+                                    System.out.println("start execution: " + new Date());
+                                    Boolean value = Boolean.valueOf(
+                                            new String(
+                                                    bytes));
+                                    System.out.println("stop execution: " + new Date());
+                                    return value;
+                                });
+                        System.out.println("there: " + new Date());
+                        return observable;
+                    });
+        };
+
+        Action1<CloseableHttpAsyncClient> disposeAction = (client) -> {
+            System.out.println(Thread.currentThread().getName() + " : Closing the client.");
+            try {
+                client.close();
+            } catch (IOException ignored) {
+            }
+        };
+
+        return Observable.using(
+                resourceFactory,
+                observableFactory,
+                disposeAction);
     }
 
     @Override
-    protected Boolean getFallback() {
-        System.out.println("We've got a problem: " + getExecutionException());
-        return false;
+    protected Observable<Boolean> construct() {
+        return request("http://localhost:" + Starter.PORT_NUMBER + "/2000");
+    }
+
+    @Override
+    protected Observable<Boolean> resumeWithFallback() {
+        System.out.println("fallback");
+        getExecutionException().printStackTrace();
+        return Observable.just(Boolean.FALSE);
     }
 }
--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,7 +1,22 @@
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.sun.org.apache.xml.internal.serializer.Encodings;
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.CharEncoding;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import rx.Observable;
+import rx.Subscription;
+import rx.subjects.BehaviorSubject;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
@@ -9,8 +24,8 @@ import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMoc
 public class Starter {
 
     public static final int PORT_NUMBER = 8082;
-    public static final int RESPONSE_DELAY = 3000;
-    public static final int HYSTRIX_TIMEOUT = 1000;
+    public static final int RESPONSE_DELAY = 1000;
+    public static final int HYSTRIX_TIMEOUT = 10000;
 
     public static void main(String[] args) {
 
@@ -21,8 +36,35 @@ public class Starter {
             Command command = new Command();
 
             System.out.println("start: " + new Date());
-            Boolean result = command.execute();
-            System.out.println("stop: " + new Date() + " " + result);
+            Observable<Boolean> result = command.toObservable();
+            System.out.println("readingObservableValue: " + new Date());
+
+//            BehaviorSubject<Boolean> booleanBehaviorSubject = BehaviorSubject.create();
+//            result.subscribe(booleanBehaviorSubject);
+//            Boolean value = booleanBehaviorSubject.getValue();
+//
+//
+            result.subscribe(next -> System.out.println("stop: " + new Date() + " " + next));
+
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+//            HttpGet httpRequest = new HttpGet("http://localhost:" + Starter.PORT_NUMBER + "/2000");
+//            HttpClient client = HttpClientBuilder.create()
+//                    .build();
+//            HttpResponse response = client.execute(httpRequest);
+//            StringWriter writer = new StringWriter();
+//            IOUtils.copy(response.getEntity().getContent(), writer, CharEncoding.UTF_8);
+//            String theString = writer.toString();
+//
+//            System.out.println(theString);
+//        } catch (ClientProtocolException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
         } finally {
             server.stop();
         }
@@ -35,7 +77,7 @@ public class Starter {
         stubFor(get(urlEqualTo("/2000"))
                 .willReturn(aResponse()
                         .withHeader("Content-Type", "text/plain")
-                        .withBody("Hey!")
+                        .withBody("true")
                         .withFixedDelay(RESPONSE_DELAY)));
     }
 }
